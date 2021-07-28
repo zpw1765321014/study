225c336ae2cd4c5b80615024aba2b3d18e3b7e05
zpw1765321014 <1765321014@qq.com>
2021-07-28 17:33:25 +0800

    互斥锁 修改注释

--- test/pthread/p7.c
+++ test/pthread/p7.c
@@ -5,9 +5,12 @@
 
 **/
 //pthread_mutex_lock; //反复枷锁会产生死锁
+
 pthread_mutex_t mute;
 int value = 0;
-void *fun(void *arg){
+
+void *fun(void *arg)
+{
     //上锁，函数是阻塞的
     pthread_mutex_lock(&mute);
     printf("now is %d and old value is %d \n",*((int *)arg), value);
@@ -17,18 +20,24 @@ void *fun(void *arg){
     pthread_mutex_unlock(&mute);
 }
 int main(){
- 
+
+    int i;
+
     pthread_t threads[5];
     int thread_id[5];
     //创建锁，相当于new一个对象
     pthread_mutex_init(&mute, NULL);
-    for(int i=0; i<5; ++i){
-        thread_id[i] = i;
+    //创建5个线程
+    for(i = 0; i<5; ++i){
+        thread_id[i] = i;   // (void *)&thread_id[i]强制转换为空指针
         pthread_create(&threads[i], NULL, fun, (void *)&thread_id[i]);
     }
-    for(int i=0; i<5; ++i)
-        int rc = pthread_join(threads[i], NULL);
+    int rc ;
+    //回收线程
+    for(i=0; i<5; ++i)
+        rc = pthread_join(threads[i], NULL);
     //释放互斥锁
     pthread_mutex_destroy(&mute);
+   
     return 0;
 }
