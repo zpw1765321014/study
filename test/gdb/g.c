a808ce66d7822e9f0f7e9c2455931e707ea36d71
admin123456 <1765321014@qq.com>
2021-08-01 21:27:55 +0800

    gdb调试

---
+++ test/gdb/g.c
@@ -0,0 +1,16 @@
+
+/**
+   gcc gdbtest.c -o a.out -g
+   gdb 调试  l  list 查看源代码成   l  num  定位到具体的行
+   b  也就是break   打断点    b 20 在20的位置设置断点
+   run  代表执行程序  缩写  r
+   next 代表 下一条指令 货越过函数  缩写 n
+   step 进入到某一行  会进入函数   缩写 s
+   print 打印变量       缩写 p
+   continue  继续执行后续指令 
+   quit 退出当前调试
+   finish 结束当前的函数调用
+   set  参数的命令
+   info b: 查看断点表
+   ptype 变量名  查看变量的类型
+**/
