de2e5c0c0ce803fedb9d0e8d7f09fb7d10283c0e
admin123456 <1765321014@qq.com>
2021-08-06 20:59:46 +0800

    协成深度理解加深

--- test/continue/c4.c
+++ test/continue/c4.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
-#include<stdlib.h>
-#include<ucontext.h>
+#include <stdlib.h>
+#include <ucontext.h>
 #include <unistd.h>
 
 /**
--- test/continue/c8.c
+++ test/continue/c8.c
@@ -7,12 +7,14 @@ ucontext_t ctx_main, ctx_f1, ctx_f2;
 
 void fun1( void ) {
 	printf("fun1() start\n");
+    //切换执行顺序
 	swapcontext(&ctx_f1, &ctx_f2);//切换至f2上下文
 	printf("fun1() end\n");
 }
 
 void fun2( void ) {
 	printf("fun2() start\n");
+    //切换执行顺序
 	swapcontext(&ctx_f2, &ctx_f1);//切换回f1上下文
 	printf("fun2() end\n");
 }
@@ -23,7 +25,7 @@ int main( void ) {
 	
 	getcontext(&ctx_f1);
 	getcontext(&ctx_f2);
-
+   /*******************修改对应的上下文信息 start********************/
 	ctx_f1.uc_stack.ss_sp    = stack1;
 	ctx_f1.uc_stack.ss_size  = 1024*8;
 	ctx_f1.uc_stack.ss_flags = 0;
@@ -35,6 +37,7 @@ int main( void ) {
 	ctx_f2.uc_stack.ss_flags = 0;
 	ctx_f2.uc_link = &ctx_main;//f2后继上下文为主流程
 	makecontext(&ctx_f2, fun2, 0);//设置入口函数
+    /*******************修改对应的上下文信息 start********************/
 
 	swapcontext(&ctx_main, &ctx_f1);//保存ctx_main,从主流程上下文切换至ctx_f1上下文
 
